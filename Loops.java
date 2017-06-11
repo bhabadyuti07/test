0aa12e5f44c14a407f4447819fdb3c63152aff92
bhabadyuti <bhabadyuti.damodar@piterion.com>
2017-06-11 12:16:30 +0530

    new files added

--- Loops.java
+++
@@ -1,9 +0,0 @@
-public class Loops{
-	
-	public static void main(String ar[]){
-		for(int i=1;i<=3;i++){
-			System.out.println("Loop..."+i);
-		}
-	}
-	
-}
--- Loops1.java
+++
@@ -1,9 +0,0 @@
-public class Loops1{
-	
-	public static void main(String ar[]){
-		for(int i=1;i<=3;i++){
-			System.out.println("Loop..."+i);
-		}
-	}
-	
-}
